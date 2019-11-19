279b119e62e1036305748c96cf0c4ce778fac205
NadaEidah <nada.eadah@gmail.com>
2019-11-19 16:04:18 +0200

    first ex

---
+++ Exerciese1.py
@@ -0,0 +1,185 @@
+#Exercises
+
+#Exercise 1
+
+print('hello\n\thello\n\t\thello')
+
+#Exercise 2
+
+print('OrangeAcademy \n'*20)
+
+#Exercise 3
+
+x=float(1)
+y=float(2.8)
+z=float("3")
+w=float("4.2")
+
+
+"""print(x)
+print(y)
+print(z)
+print(w)"""
+
+print(x,y,z,w,sep='10')
+
+#to have 10 between the value
+
+
+#Exercise 4
+
+number1=input("Enter the first number?")
+number2=input("Enter the second number?")
+e4=int(number1)*int(number2)
+print(e4)
+
+#Exercise 5
+
+nubr1=int(input("Enter the number 1?"))
+nubr2=int(input("Enter the number 2?"))
+nubr3=int(input("Enter the number 3?"))
+nubr4=int(input("Enter the number 4?"))
+nubr5=int(input("Enter the number 5?"))
+
+print((nubr1+nubr2+nubr3+nubr4+nubr5)/5)
+
+#Exercise 6
+
+x=1
+y=2.8
+z=1j
+o="Orange"
+A=True
+
+print(type(x)) #int
+print(type(y)) #flot
+print(type(z)) #complex
+print(type(o)) #str
+print(type(A)) #bool
+
+"""<class 'int'>
+<class 'float'>
+<class 'complex'>
+<class 'str'>
+<class 'bool'>"""
+
+#Exercise 7
+
+x,y="Orange",1
+X1,Y1=100,-10
+print(x)
+print(y)
+print(X1)
+print(Y1)
+
+"""
+Orange
+1
+100
+-10
+"""
+
+#Exercise 8
+
+"""
+5x=10
+print(x5)
+X_q$="orange"
+print(X_q$)
+A B(14)
+print(A B)
+Print("10"*10)
+"""
+x5=10 # cannot use in the beginning number
+print(x5)
+X_q="orange" # cannot use in the $ in name of Value
+print(X_q)
+AB=14  # cannot use in the space in name of Value
+print(AB)
+print(10*10) # cannot use print in capital p and the ""
+
+#Exercise 9
+
+x,y="Orange",1
+X1,Y1=100,-10
+print(x)
+print(y)
+print(X1)
+print(Y1)
+
+"""
+Orange
+1
+100
+-10
+"""
+
+#Exercise 10
+
+"""
+5x=10
+print(x5)
+X_q$="orange"
+print(X_q$)
+A B(14)
+print(A B)
+Print("10"*10)
+"""
+x5=10 # cannot use in the beginning number
+print(x5)
+X_q="orange" # cannot use in the $ in name of Value
+print(X_q)
+AB=14  # cannot use in the space in name of Value
+print(AB)
+print(10*10) # cannot use print in capital p and the ""
+
+#Exercise 11
+
+#print("Hello,World!")
+print("Cheers,Mate!")#This is the program
+
+"""Testing
+The Code
+"""
+# the output is :- Cheers,Mate!
+
+#Exercise 12
+
+name=input("Enter Name?? ")
+age=int(input("Enter Age (year)?? "))
+e12=2119-age
+print('my age after 100 years '+str(e12))
+
+
+#Exercise 13
+
+base=int(input("what is base?? "))
+height=int(input("what is height?? "))
+
+e13=base*height/2
+
+print(e13)
+
+
+#Exercise 14
+
+x=11
+y=3
+
+print(x+y)              #14
+print(x-y)              #8
+print(x*y)              #33
+print(x/y)              #3.6666666666666665
+print(x//y)             #3 
+print(x%y)              #2
+print(abs(x*-1))        #11
+print(int(x))           #11
+print(float(x))         #11.0
+print(divmod(x,y))      #(3,2)
+print(pow(x,y))         #1331
+print(x**y)            #1331
+print(x>y)             #True
+print(x==y)           #False
+print(x!=y)          #True
+
+
